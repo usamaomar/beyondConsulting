5c3e000f5291f3e46a03cd4aeb52723214eff5e4
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-15 15:32:48 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/index.dart
+++ lib/custom_code/widgets/index.dart
@@ -3,3 +3,4 @@ export 'panner_progress_par_custom_widget.dart'
 export 'circular_progress_par_custom_widget.dart'
     show CircularProgressParCustomWidget;
 export 'sync_fusion_flutter_bar_charts.dart' show SyncFusionFlutterBarCharts;
+export 'project_tracker_custom_widget.dart' show ProjectTrackerCustomWidget;
---
+++ lib/custom_code/widgets/project_tracker_custom_widget.dart
@@ -0,0 +1,33 @@
+// Automatic FlutterFlow imports
+import '/backend/schema/structs/index.dart';
+import '/backend/schema/enums/enums.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+class ProjectTrackerCustomWidget extends StatefulWidget {
+  const ProjectTrackerCustomWidget({
+    Key? key,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  final double? width;
+  final double? height;
+
+  @override
+  _ProjectTrackerCustomWidgetState createState() =>
+      _ProjectTrackerCustomWidgetState();
+}
+
+class _ProjectTrackerCustomWidgetState
+    extends State<ProjectTrackerCustomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
